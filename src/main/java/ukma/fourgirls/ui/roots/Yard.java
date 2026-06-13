fb6d44acbbfbfedb9881dc318404392cae4d8f63
Tetiana Soltys <soltetiana12@gmail.com>
2026-06-13 13:51:16 +0300

    feat: add images and finalize lor in Yard

--- src/main/java/ukma/fourgirls/ui/roots/Yard.java
+++ src/main/java/ukma/fourgirls/ui/roots/Yard.java
@@ -1,12 +1,17 @@
 package ukma.fourgirls.ui.roots;
 
 import javafx.animation.FadeTransition;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
+import ukma.fourgirls.core.AudioManager;
+import ukma.fourgirls.core.SceneManager;
 import ukma.fourgirls.logic.StoryRunner;
 import ukma.fourgirls.ui.CameraController;
+import ukma.fourgirls.ui.CharacterView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +19,7 @@ import java.util.Map;
 public class Yard extends Place {
     private static final String IMAGE_PATH = "/images/yard.png";
     private final Rectangle blackOverlay;
+    private CharacterView actorView;
 
     public Yard() {
         super(IMAGE_PATH);
@@ -31,11 +37,11 @@ public class Yard extends Place {
     @Override
     public void onEnter() {
         CameraController.setPanningEnabled(false);
-
         this.startYardRavenCutscene();
     }
 
     private void startYardRavenCutscene() {
+        actorView = new CharacterView((StackPane) this.getRoot());
         Map<String, Runnable> actions = new HashMap<>();
 
         actions.put("show_yard_view", () -> {
@@ -45,9 +51,32 @@ public class Yard extends Place {
             fadeInYard.play();
         });
 
+        actions.put("showSadYevdokha", () -> {
+            if (actorView != null) {
+                actorView.setPositionSide(true);
+                actorView.setCharacterSprite("/images/Zasmuchena_evdoha.png");
+            }
+        });
+
+        actions.put("showHappyYevdokha", () -> {
+            if (actorView != null) {
+                actorView.setPositionSide(true);
+                actorView.setCharacterSprite("/images/happy_Yevdokha.png");
+            }
+        });
+
+        actions.put("hideActor", () -> {
+            if (actorView != null)
+                actorView.hide();
+        });
+
         actions.put("enable_papyrus_pickup", () -> {
             // Тут ти потім налаштуєш клік по папірусу через InventoryManager, як у ChildRoom!
             this.enableNavigation();
+        });
+
+        actions.put("go_to_forest_automatically", () -> {
+            SceneManager.getInstance().switchToCachedRoom("Forest", Forest::new);
         });
 
         StoryRunner.playScene("/story/chapter2.json", "yard_raven_scene", (StackPane) this.getRoot(), actions, null);
